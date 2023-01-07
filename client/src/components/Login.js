eefaa98261bed57069a6d19ab5404b48c031702d
ali asgari <aamining@gmail.com>
2023-01-07 12:42:19 +1100

    try to fix cors

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -18,6 +18,7 @@ class Login extends Component{
           method:'post',
           //in development:
           url:'http://localhost:7000/users/login',
+          headers: {"Access-Control-Allow-Origin": `https://lbtest.herokuapp.com/users/login`},
 
           //in production:
           //url:'https://commentsforali.herokuapp.com/users/login',
